98ee22ea0f8af2323b5cf1b3f20dcd040e5bcd37
DoanHieu <doanhieu.bl.hn@gmail.com>
2019-11-12 17:19:09 +0700

    count timer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CountDownTimerComponent } from './count-down-timer/count-down-timer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountDownTimerComponent
   ],
   imports: [
     BrowserModule
